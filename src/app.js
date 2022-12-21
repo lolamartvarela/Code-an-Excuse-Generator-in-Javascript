86753380688c9ba54b27be95504a487a7c595c79
lolamartvarela <luciamvarela@outlook.com>
2022-12-21 15:19:17 +0000

    Generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+};
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let getRandomInt1 = Math.floor(Math.random() * who.length);
+  let getRandomInt2 = Math.floor(Math.random() * action.length);
+  let getRandomInt3 = Math.floor(Math.random() * what.length);
+  let getRandomInt4 = Math.floor(Math.random() * when.length);
+
+  return (
+    who[getRandomInt1] +
+    " " +
+    action[getRandomInt2] +
+    " " +
+    what[getRandomInt3] +
+    " " +
+    when[getRandomInt4]
+  );
 };
